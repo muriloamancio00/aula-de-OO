e83229734ba007a7abde45e2a96a74017fd8ac3c
murilo <61051754+muriloamancio00@users.noreply.github.com>
2020-02-21 11:09:39 -0300

    Teste 1 a 2500
    terminando..

---
+++ TesteNumeros.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class TesteNumeros{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int[] numeros new int [20] ;
+        int cont=0 ;
+        float i;
+        for (i=0;i<=2500;i++){
+            for(;i/5==0;i++){
+                System.out.println("%f", i);
+            }
+        }
+
+    }
+}
